b083fb0ffecdef45429e7c464b98ffd46b21f51e
BuggyAl <64368488+BuggyAl@users.noreply.github.com>
2022-03-18 09:12:53 -0500

    added a radical simplifier

--- Geometry/PythagoreanTheorem.js
+++ Geometry/PythagoreanTheorem.js
@@ -3,6 +3,30 @@ function pythagoreanTheorem() {
     const num2 = eval(String(prompt("Please enter the expression of leg #2!")));    
     const root = (num1 * num1) + (num2 * num2);    
     var ans = Math.sqrt(root);    
-    alert("The answer is: " + ans + " (√" + root + ")");
+    const simplified = simplifyRadical(root);
+    alert("The answer is: " + ans + " (" + simplified + ")");
 }
 pythagoreanTheorem();
+
+function simplifyRadical(full) {    
+    let maxSquareFactor = 0;
+    let outer = 0;
+    let inner = 0;
+    for (let i = 0; i < full; i++) {
+        if (!(isWhole(full / i))) { continue; }
+        if (!(isSquare(i))) { continue; }
+        if (!(i > maxSquareFactor)) {continue;}
+        maxSquareFactor = i;
+        outer = Math.sqrt(maxSquareFactor);
+        inner = full / i;
+    }
+    return outer + "√" + inner;
+}
+
+function isSquare (n) {
+    return Math.sqrt(n) % 1 == 0;
+}
+
+function isWhole(n) {
+    return Math.floor(n) == n;
+}
