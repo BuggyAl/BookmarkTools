f09876812c18cda05752ff8be0635a4ad0d9a8d4
BuggyAl <64368488+BuggyAl@users.noreply.github.com>
2022-03-18 08:50:42 -0500

    updates + law of cosine

--- Geometry/PythagoreanTheorem.js
+++ Geometry/PythagoreanTheorem.js
@@ -1,6 +1,34 @@
 function pythagoreanTheorem() {    
     const num1 = eval(String(prompt("Please enter the expression of leg #1!")));
     const num2 = eval(String(prompt("Please enter the expression of leg #2!")));
-    alert("The answer is: " + Math.sqrt(root) + " (√" + (num1 * num1) + (num2 * num2) + ")");
+
+    if (!isWhole(Math.sqrt(root))) {
+        simplifyRadical(root);
+    } else {
+        alert("The hypontenuse is: " + Math.sqrt(root) + " (√" + (num1 * num1) + (num2 * num2) + ")");
+    }
 }
 pythagoreanTheorem();
+
+function simplifyRadical(full) {    
+    let maxSquareFactor = 0;
+    let outer = 0;
+    let inner = 0;
+    for (let i = 0; i < full; i++) {
+        if (!(isWhole(full / i))) { continue; }
+        if (!(isSquare(i))) { continue; }
+        if (!(i > maxSquareFactor)) {continue;}
+        maxSquareFactor = i;
+        outer = Math.sqrt(maxSquareFactor);
+        inner = full / i;
+    }
+    alert("The hypotenuse is: " + outer + "√" + inner);
+}
+
+function isSquare (n) {
+    return Math.sqrt(n) % 1 == 0;
+}
+
+function isWhole(n) {
+    return Math.floor(n) == n;
+}
---
+++ Geometry/Trigonometry/LawCos.js
@@ -0,0 +1,15 @@
+function lawCos() {    
+    const a = eval(String(prompt("Please enter the expression of leg #1!")));
+    const b = eval(String(prompt("Please enter the expression of leg #2!")));
+    const angC = eval(String(prompt("Please enter angle C!")));
+    
+    const cos = Math.cos(angC * Math.PI / 180);
+
+    const a2 = a * a;
+    const b2 = b * b;
+
+    const ans = a2 + b2 - (2 * a * b * cos);
+
+    alert("The answer is: " + Math.sqrt(ans));
+}
+lawCos();
