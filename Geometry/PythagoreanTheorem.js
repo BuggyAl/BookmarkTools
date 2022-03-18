658c353f58e37dddd666600b3cb6d3e31d8f5e28
BuggyAl <64368488+BuggyAl@users.noreply.github.com>
2022-03-18 09:05:20 -0500

    fixed var

--- Geometry/PythagoreanTheorem.js
+++ Geometry/PythagoreanTheorem.js
@@ -1,5 +1,6 @@
 function pythagoreanTheorem() {        
     const num1 = eval(String(prompt("Please enter the expression of leg #1!")));    
     const num2 = eval(String(prompt("Please enter the expression of leg #2!")));    
-    const root = (num1 * num1) + (num2 * num2);    var ans = Math.sqrt(root);    
+    const root = (num1 * num1) + (num2 * num2);    
+    var ans = Math.sqrt(root);    
     alert("The answer is: " + ans + " (√" + root + ")");}pythagoreanTheorem();
